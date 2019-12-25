a8925b1db52803867101ef5565c245b0fc57577c
Md Rayhan <mdrayhan9464@gmail.com>
2019-12-25 15:53:55 +0600

    Program opened

---
+++ string_practice/src/main/java/org/richit/string_practice/MyClass.java
@@ -0,0 +1,19 @@
+package org.richit.string_practice;
+
+public class MyClass {
+
+    static String dot = "----------------";
+    static String start = "Programme started";
+    static String end = "Programme ended";
+
+    public static void main(String[] args) {
+        System.out.println(dot+start+dot);
+        System.out.println("");
+
+        System.out.println("Programme started");
+
+        System.out.println("");
+        System.out.println(dot+end+dot);
+    }
+
+}
